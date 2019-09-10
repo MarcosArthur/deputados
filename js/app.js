5a62b23a707fe397f2bb2bfac4cddcab75ef0af7
Marcos Arthur <cruzartur61@gmail.com>
2019-09-10 14:23:56 -0300

    implementação do grafico e table de gastos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 
+
 let load = document.querySelector('.load');
 
 function handleErrors(response) {
@@ -28,7 +29,7 @@ fetch('https://dadosabertos.camara.leg.br/api/v2/deputados', {
                         <span>Partido ${e.siglaPartido}</span>
                         <span>UF: ${e.siglaUf}</span>
                         <a href="mailto:${e.email}">${e.email}</a>
-                        <button class="btn" data-id="${e.id}">Gastos</button>
+                        <button class="btn button" data-id="${e.id}">Gastos</button>
                     </div>
                 </div>
                 `;
@@ -43,7 +44,7 @@ fetch('https://dadosabertos.camara.leg.br/api/v2/deputados', {
    })
 
 function button() {
-   let button = document.querySelectorAll('button');
+   let button = document.querySelectorAll('.button');
    let modal = document.querySelector('.modal');
    button.forEach(e => {
       e.addEventListener('click', event => {
@@ -58,14 +59,92 @@ function button() {
             .then(response => response.json())
             .then(response => {
                console.log(response)
-                if (response) modal.classList.add('active');
-               let valor  = 0;
+               if (response) modal.classList.add('active');
+               let mes = [];
+               let gastos = [];
+
+               let table = " ";
+
                response.dados.forEach(e => {
-                  valor = valor + e.valorDocumento;
+                  mes.push(e.mes);
+                  gastos.push(e.valorLiquido);
+                  table+= `
+                     <table class="table-info" c >
+                        <thead>
+                           <tr>
+                              <td>
+                                 Tipo Despesa
+                              </td>
+                              <td>
+                                 Ano
+                              </td>
+                              <td>
+                                Mês
+                              </td>
+                              <td>
+                                 Nome Fornecedor
+                              </td>
+                              <td>
+                                 Valor
+                              </td>
+                           </tr>
+                        </thead>
+                        <tbody>
+                           <tr>
+                              <td>
+                                 ${e.tipoDespesa}
+                              </td>
+
+                              <td>
+                                 ${e.ano}
+                              </td>
+                              <td>
+                                 ${e.mes}
+                              </td>
+
+                              <td>
+                                 ${e.nomeFornecedor}
+                              </td>
+
+                              <td>
+                                R$ ${e.valorLiquido}
+                              </td>
+
+                           </tr>
+                        </tbody>
+                     <table>
+                  `;
                })
 
-               console.log(Math.round(valor))
+               var ctx = document.getElementById('myChart').getContext('2d');
+               var chart = new Chart(ctx, {
+                   
+                   type: 'line',
+               
+                   
+                   data: {
+                       labels:mes,
+                       datasets: [{
+                           label: 'Gastos Deputado(a)',
+                           backgroundColor: 'rgb(255, 99, 132)',
+                           borderColor: 'red',
+                           data: gastos
+                       }]
+                   },
+               
+                   options: {
+                    }
+               });
+
+
+               document.querySelector('.table-mount').innerHTML = table;
+               
             })
       });
    })
 }
+
+
+document.querySelector('.close').addEventListener('click', function() {
+   document.querySelector('.modal').classList.remove('active');
+})
